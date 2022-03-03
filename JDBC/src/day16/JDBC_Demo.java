f539505541f3a19993f8f32820223066baadfd07
YogeeshYogi007 <yogeeshgowda476@gmail.com>
2022-03-03 18:01:24 +0530

    13th ommit

--- JDBC/src/day16/JDBC_Demo.java
+++ JDBC/src/day16/JDBC_Demo.java
@@ -2,6 +2,7 @@ package day16;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,20 +14,27 @@ public class JDBC_Demo {
 			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sample","Yogeesh","tiger");
 			Statement st=con.createStatement();
 			//to create a table in student
-			String query="create table student (ID int primary key,SName varchar(20), SMarks int)";
+			//String query="create table student (ID int primary key,SName varchar(20), SMarks int)";
 			//to insert values into column
-			String query1= "insert into student values (1,'yogi',20)";
+			String query1= "insert into student values (6,'Sam',20)";
 			//to update values in column
-			String query2= "update student set SName='Yogi' where ID=1";
+			//String query2= "update student set SName='Yogi' where ID=1";
 			//to create table using alter
-			String query3= "alter table student add (percentage double)";
+			//String query3= "alter table student add (percentage double)";
 			//to rename column using alter
-			String query4= "alter table student rename column percentage to CGPA";
+			//String query4= "alter table student rename column percentage to CGPA";
 			//to drop column using alter
-			String query5= "alter table student drop CGPA";
+			//String query5= "alter table student drop CGPA";
 			//to delete any row
-			 String query6= "delete from  student where ID=1";
-			st.executeUpdate(query);
+			 //String query6= "delete from  student where ID=1";
+			ResultSet rs=st.executeQuery("SELECT ID,SName,SMarks FROM student");
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1));
+				System.out.println(rs.getString(2));
+				System.out.println(rs.getString(3));
+			}
+			st.executeUpdate(query1);
 			//st.executeUpdate(query1);
 			///st.executeUpdate(query2);
 			//st.executeUpdate(query3);
@@ -34,12 +42,12 @@ public class JDBC_Demo {
 			//st.executeUpdate(query5);
 			//st.executeUpdate(query6);
 			//System.out.println("Students Table created successfully");
-			//System.out.println("Row inserted succesfully");
+			//System.out.println("Row inserted successfully");
 			//System.out.println("updated successfull"); 
 			//System.out.println("new column added using alter successfull"); 
 			//System.out.println("Rename column using alter successfull");
 			//System.out.println("Drop column using alter successfull");
-			System.out.println("deleted succesfully");
+			//System.out.println("deleted succesfully");
 			st.close();
 			con.close();
 			
